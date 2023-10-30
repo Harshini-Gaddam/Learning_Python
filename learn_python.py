d2db5237a8e856bca7eeeae1f4471e3058504c86
Harshini-Gaddam <harshini.gaddam2209@gmail.com>
2023-10-30 14:53:12 -0400

    added new py file

---
+++ learn_python.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])  # Access an element
+
+fruits.append("orange")  # Add an element
+print(fruits)
+fruits.insert(2, "strawberry")
+print(fruits)
