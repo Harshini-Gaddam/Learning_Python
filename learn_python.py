50365f15185184e3066da9f408a968cbdfda4af2
Harshini-Gaddam <harshini.gaddam2209@gmail.com>
2023-10-30 14:58:55 -0400

    made some changes

--- learn_python.py
+++ learn_python.py
@@ -1,3 +1,4 @@
+'''
 fruits = ["apple", "banana", "cherry"]
 print(fruits[1])  # Access an element
 
@@ -5,3 +6,16 @@ fruits.append("orange")  # Add an element
 print(fruits)
 fruits.insert(2, "strawberry")
 print(fruits)
+'''
+
+for i in range(5):
+    print(i)
+print("done")
+sum=0
+mul=1
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+    sum=sum+num
+    mul=mul*num
+print(sum, mul)
+
